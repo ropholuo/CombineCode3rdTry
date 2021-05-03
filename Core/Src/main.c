4b6a5ab4c7976c49fbda93392bfbc8b80652ccd2
ropholuo <78669543+ropholuo@users.noreply.github.com>
2021-05-02 19:33:15 -0700

    new rtc functions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -57,6 +57,9 @@ TIM_HandleTypeDef htim6;
 
 UART_HandleTypeDef huart1;
 
+RTC_DateTypeDef gDate;
+RTC_TimeTypeDef gTime;
+
 /* USER CODE BEGIN PV */
 
 
@@ -159,21 +162,37 @@ int main(void)
 
 /**
  *
-  * @brief System Clock Configuration
-  * @retval None
+  * @brief get the rtc time and date and format both in strings
   */
 void get_time(void)
 {
- RTC_DateTypeDef gDate;
- RTC_TimeTypeDef gTime;
-/* Get the RTC current Time */
- HAL_RTC_GetTime(&hrtc, &gTime, RTC_FORMAT_BIN);
-/* Get the RTC current Date */
- HAL_RTC_GetDate(&hrtc, &gDate, RTC_FORMAT_BIN);
-/* Display time Format: hh:mm:ss */
- sprintf((char*)time,"%02d:%02d:%02d",gTime.Hours, gTime.Minutes, gTime.Seconds);
-/* Display date Format: dd-mm-yy */
- sprintf((char*)date,"%02d-%02d-%2d",gDate.Date, gDate.Month, 2000 + gDate.Year);
+	 RTC_DateTypeDef gDate;
+	 RTC_TimeTypeDef gTime;
+	/* Get the RTC current Time */
+	 HAL_RTC_GetTime(&hrtc, &gTime, RTC_FORMAT_BIN);
+	/* Get the RTC current Date */
+	 HAL_RTC_GetDate(&hrtc, &gDate, RTC_FORMAT_BIN);
+	/* Display time Format: hh:mm:ss */
+	 sprintf((char*)time,"%02d:%02d:%02d",gTime.Hours, gTime.Minutes, gTime.Seconds);
+	/* Display date Format: dd-mm-yy */
+	 sprintf((char*)date,"%02d-%02d-%2d",gDate.Date, gDate.Month, 2000 + gDate.Year);
+}
+
+uint8_t check_time(void)
+{
+	char clock[5];
+
+	/* Get the RTC current Time */
+	 HAL_RTC_GetTime(&hrtc, &gTime, RTC_FORMAT_BIN);
+	/* Get the RTC current Date */
+	 HAL_RTC_GetDate(&hrtc, &gDate, RTC_FORMAT_BIN);
+
+	 sprintf((char*)clock,"%02d:%02d:%02d",gTime.Hours, gTime.Minutes);
+	 if(clock == mtime)
+		 	 	 return 1;
+	 else return 0;
+
+	 // didn't check the weekday yet
 }
 
 void set_alarm(char timearr[], char weekday[], int apm)
@@ -271,6 +290,11 @@ void set_time(char arr)
 	    Error_Handler();
 	  }
 }
+/**
+ *
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
